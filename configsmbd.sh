3587fce8f5ff7fda88455a8765a8a9552e1817d6
Alvarojorgeg <145665238+Alvarojorgeg@users.noreply.github.com>
2024-03-01 19:58:32 +0100

    Update configsmbd.sh

--- configsmbd.sh
+++ configsmbd.sh
@@ -27,95 +27,98 @@ if [ $# -eq 0 ]; then
 				printf "\n"
 				read -p "Elige una opción" opcion
 				case $opcion in
-				1)
-					printf "\n"
-					echo "Configuración Interactiva de smb.conf"
-					echo "------------------------------------"
-					
-					# Solicitar el nombre del usuario
-					read -p "Nombre del usuario: " usuario
-					# Solicitar la contraseña del usuario
-					read -p "Contraseña: " contra
-					# Solicitar la dirección IP
-					read -p "Dirección IP: " ip
-					# Solicitar el nombre del grupo de trabajo
-					read -p "Nombre del Grupo de Trabajo: " workgroup
-					# Solicitar el nombre NetBIOS
-					read -p "Nombre NetBIOS del Servidor: " netbios_name
-					# Solicitar la descripción del servidor
-					read -p "Descripción del Servidor: " server_string
-					# Solicitar el nombre del directorio compartido
-					read -p "Nombre del Directorio Compartido: " share_name
-					# Solicitar la ruta del directorio compartido
-					read -p "Ruta del Directorio Compartido: " share_path
-					# Solicitar la lista de usuarios válidos
-					read -p "Usuarios Válidos (separados por comas): " valid_users					
-					# Crear el archivo smb.conf
-					cat <<EOL > /etc/samba/smb.conf
-					[global]
-					   workgroup = $workgroup
-					   netbios name = $netbios_name
-					   server string = $server_string
+					1)
+						printf "\n"
+						echo "Configuración Interactiva de smb.conf"
+						echo "------------------------------------"
+						
+						# Solicitar el nombre del usuario
+						read -p "Nombre del usuario: " usuario
+						# Solicitar la contraseña del usuario
+						read -p "Contraseña: " contra
+						# Solicitar la dirección IP
+						read -p "Dirección IP: " ip
+						# Solicitar el nombre del grupo de trabajo
+						read -p "Nombre del Grupo de Trabajo: " workgroup
+						# Solicitar el nombre NetBIOS
+						read -p "Nombre NetBIOS del Servidor: " netbios_name
+						# Solicitar la descripción del servidor
+						read -p "Descripción del Servidor: " server_string
+						# Solicitar el nombre del directorio compartido
+						read -p "Nombre del Directorio Compartido: " share_name
+						# Solicitar la ruta del directorio compartido
+						read -p "Ruta del Directorio Compartido: " share_path
+						# Solicitar la lista de usuarios válidos
+						read -p "Usuarios Válidos (separados por comas): " valid_users					
+						# Crear el archivo smb.conf
+						cat <<EOL | sudo tee /etc/samba/smb.conf
+[global]
+   workgroup = $workgroup
+   netbios name = $netbios_name
+   server string = $server_string
 
-					[$share_name]
-					   path = $share_path
-					   browseable = yes
-					   writable = yes
-					   valid users = $valid_users
-					EOL
+[$share_name]
+   path = $share_path
+   browseable = yes
+   writable = yes
+   valid users = $valid_users
+EOL
 
-					echo "Configuración completada. Se ha creado /etc/samba/smb.conf con la configuración proporcionada."
-					# Reiniciar el servicio Samba
-					sudo systemctl restart smbd
-					while true
-					do
-					echo "a. Instalar con comandos"
-					echo "b. Instalar con Ansible"
-					echo "c. Instalar con Docker"
-					read -p "Instalar el servicio con: "  instalar
-					case $instalar in
-					a)
-						ssh $user@$ip
-						sudo apt update
-						sudo apt install samba
-						sudo systemctl status smbd
+						echo "Configuración completada. Se ha creado /etc/samba/smb.conf con la configuración proporcionada."
+						# Reiniciar el servicio Samba
+						sudo systemctl restart smbd
+						while true
+						do
+							echo "a. Instalar con comandos"
+							echo "b. Instalar con Ansible"
+							echo "c. Instalar con Docker"
+							read -p "Instalar el servicio con: "  instalar
+							case $instalar in
+								a)
+									ssh $user@$ip
+									sudo apt update
+									sudo apt install samba
+									sudo systemctl status smbd
+								;;
+								b)
+									echo "$ip ansible_ssh_user=$usuario ansible_ssh_pass=$contra" >> ./host
+									ansible-playbook -v samba.yml --extra-vars "ansible_sudo_pass=$pass"
+								;;
+								c)
+									echo "opcion c"
+								;;
+								*)
+									exit 0
+								;;
+							esac
+						done
+						printf "\n"
 					;;
-	                                b)
-	                                        echo "$ip ansible_ssh_user=$usuario ansible_ssh_pass=$contra >> ./host
-						ansible-playbook -v samba.yml --extra-vars "ansible_sudo_pass=$pass"
-	                                ;;
-	                                c)
-	                                        echo "opcion c"
-	                                ;;
-	                                *)
-	                                        exit 0
-	                                ;;
-					esac
-				done
-				printf "\n"
-			;;
-			2)
-                                printf "\n"
-                                echo "2"
-                                printf "\n"
-
-		        ;;
-			3)
-                                printf "\n"
-                                echo "3"
-                                printf "\n"
-		        ;;
-			4)
-                                printf "\n"
-                                echo "4"
-                                printf "\n"
-
-		        ;;
-			5)
-		                exit 0
-		        ;;
-		esac
-	done
+					2)
+						printf "\n"
+						echo "2"
+						printf "\n"
+					;;
+					3)
+						printf "\n"
+						echo "3"
+						printf "\n"
+					;;
+					4)
+						printf "\n"
+						echo "4"
+						printf "\n"
+					;;
+					5)
+						exit 0
+					;;
+				esac
+			done
+		;;
+		*)
+			echo "Opción no válida"
+		;;
+	esac
 else
 	echo "con parametros"
 fi
